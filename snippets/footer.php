07bccf458abe127a4a3fd2794de82c104fa18f5f
lucadallaire <luca.dallaire@gmail.com>
2022-02-15 13:33:38 -0500

    Implemented PHP

---
+++ snippets/footer.php
@@ -0,0 +1,19 @@
+<div class="foot">
+    <ul>
+        <li class="footer-head-list-item">Store</li>
+        <br />
+        
+    </ul>
+    <ul>
+        <li class="footer-head-list-item">Account</li>
+        <br />
+        <li><a href="/account/login-page.php">Login</a></li>
+        <li><a href="/account/create-account.php">Create Account</a></li>
+    </ul>
+    <ul>
+        <li class="footer-head-list-item">Contact Us</li>
+        <br />
+        <li><a href="">Call</a></li>
+        <li><a href="/contact.php">Email</a></li>
+    </ul>
+</div>
---
+++ snippets/navbar.php
@@ -0,0 +1,17 @@
+<?php
+    session_start();
+?>
+<header>
+    <div class="nav-wrapper">
+        <nav>
+            <div class="menu-wrapper">
+                <ul class="menu">
+                    <li class="menu-item"><a href="">Products</a></li>
+                    <li class="menu-item"><a href="/account/login-page.php">Account</a></li>
+                    <li class="menu-item"><a href="/cart.php">Cart</a></li>
+
+                </ul>
+            </div>
+        </nav>
+    </div>
+</header>
