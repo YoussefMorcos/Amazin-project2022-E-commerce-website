7d56c5c4157defe485ba9e672c1cc56df374bf15
z4hed <zhedthegreat@gmail.com>
2022-04-02 21:01:24 -0400

    Improved my Orders page
    Added headers to the table - Issue #50

--- account/myOrders.php
+++ account/myOrders.php
@@ -6,6 +6,23 @@
     <link rel="stylesheet" href="../Application/css/menu-bar.css"/>
     <link rel="stylesheet" href="../Application/css/footer.css"/>
     <link rel="stylesheet" href="../Application/css/product-landing.css"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tw-elements/dist/css/index.min.css" />
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script>
+    tailwind.config = {
+        theme: {
+          extend: {
+            fontFamily: {
+            sans: ['Inter', 'sans-serif'],
+            },
+        }
+        }
+    }
+    </script>
+
+    <script src="https://cdn.jsdelivr.net/npm/tw-elements/dist/js/index.min.js"></script>
     <title>STORE</title>
 </head>
 <body>
@@ -13,6 +30,43 @@
 <?php
 include ('../navbar.php');
 
+//"My orders heading"
+$heading1 = "<h1 class=\"font-medium leading-tight text-5xl mt-1 mb-2  bg-[#59ACB3] text-white\" style=\"height: 300px;text-align: center;\">
+            <div style=\"height: 100px;\"></div>
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;My Orders
+            </h1>
+";
+
+//Table heading
+$order_table_headings = "<a class=\"landing-item__link\" >
+<div class=\"landing-item\"
+    <div class=\"landing-item_content\">
+        <div class=\"landing-item_content--header\">
+            <div class=\"landing-item_content--header-name\">
+                <p class=\"item--title\">
+                              Order ID           
+                </p>
+
+            </div>
+
+            <div style='position: absolute;right: 20px' class=\"landing-item_content--header-price\">
+                <h3 class=\"item--price\">
+                       Final Cost            
+                </h3>                                            
+            </div>
+
+        </div>
+        <h3 style='position: absolute;left: 50%' class=\"item--desc\">
+                  Date Ordered                
+        </h3>
+                           
+    </div>
+                        
+</div>
+</a>";
+
+echo $heading1;
+echo $order_table_headings;
 
 $id = $_SESSION['id'];
 $db = mysqli_connect("localhost", "root", "321trewq", "amazin", "3306") or die ("fail");
@@ -51,7 +105,9 @@ while($row = mysqli_fetch_assoc($result) and $row2 = mysqli_fetch_assoc($result2
                                </div>
                            </a>";
     echo $item;
+   
 }
 
+?>
 
-
+</body>
